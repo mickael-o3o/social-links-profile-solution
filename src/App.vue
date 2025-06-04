b80af6abffe4fd81dc7a9ec2be0aa918cf254d05
mickael-o3o <mickael.00@outlook.com>
2025-06-04 10:39:00 +0300

    finished the footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import MainSection from './components/MainSection.vue'
+import FooterSection from './components/FooterSection.vue'
 </script>
 
 <template>
   <MainSection />
+  <FooterSection />
 </template>
---
+++ src/components/FooterSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <div class="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">Mickael-o3o</a>.
+    </div>
+  </footer>
+</template>
+
+<style>
+.attribution {
+  font-size: 11px;
+  text-align: center;
+  background-color: var(--color-grey-900);
+  padding: 2rem;
+  font-family: monospace;
+}
+.attribution a {
+  color: hsl(228, 45%, 44%);
+}
+</style>
